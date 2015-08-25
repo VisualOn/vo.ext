c1898088c0ab28e9f046b511db70a323c627aa1e
Michael Kriese <michael.kriese@visualon.de>
2015-08-25 09:14:22 +0200

    Fetch msbuild path from registry.

--- build.ps1
+++ build.ps1
@@ -14,6 +14,23 @@ $out = Join-Path (Resolve-Path .) bin
 
 #region functions
 
+function Get-MsBuild
+{
+    $msbuilds = @('14.0', '12.0', '4.0')
+
+    foreach($ver in $msbuilds)
+    {
+        $r = ('HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\{0}' -f $ver)
+        if(Test-Path $r)
+        {
+            $p = $r | Get-ItemProperty -Name 'MSBuildToolsPath' | Select-Object -ExpandProperty 'MSBuildToolsPath'
+            return "$p\msbuild.exe"
+        }
+    }
+
+    throw "MsBuild not found"
+}
+
 function Invoke-MSBuild() {
 param(
 #        [Parameter(Mandatory=$true)]
@@ -26,7 +43,7 @@ param(
 
     $opts = @('/nologo', '/m', '/nr:false', "/v:m", "/t:$Target", "/p:$props", $SolutionPath);
 
-    & msbuild $opts
+    & (Get-MsBuild) $opts
 }
 
 #endregion
