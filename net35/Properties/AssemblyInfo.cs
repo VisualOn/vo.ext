13c5247ac469db5e99da76f6c352f621bd039117
Michael Kriese <michael.kriese@visualon.de>
2017-01-19 18:48:28 +0100

    Refactored project.

--- net35/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ebf1b6b4-534a-4aeb-8f8d-c6a2dc73487c")]
--- net45/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0b211026-48b6-4e3d-bae4-2ae76251ca0f")]
